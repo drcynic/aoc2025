dc30afa91cfd96191ca6a6434ae86135ffa3c83e
cynic <daniel@szymanski.cc>
2025-12-01 21:42:07 +0100

    Tidy up day 01

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,29 +1,21 @@
-use itertools::Itertools;
-use num_traits::sign;
 use std::fs;
 
 fn main() {
-    let filename = "input2.txt";
-    let input = fs::read_to_string(filename).unwrap();
-    let mut pos = 50;
-    let part1 = input
+    let part1 = fs::read_to_string("input2.txt")
+        .unwrap()
         .trim()
         .split("\n")
-        .map(|x| {
+        .fold((50i32, 0), |(pos, count), x| {
             let d = if x.starts_with("L") { -1 } else { 1 };
-            let off = x[1..].parse::<i32>().unwrap();
-            pos += off * d;
-            pos = pos.rem_euclid(100);
-            pos
+            let off = x[1..].parse::<i32>().unwrap() * d;
+            let pos = (pos + off).rem_euclid(100);
+            (pos, count + if pos == 0 { 1 } else { 0 })
         })
-        .filter(|&x| x == 0)
-        .count();
+        .1;
     println!("{:?}", part1);
 
-    let filename = "input2.txt";
-    let input = fs::read_to_string(filename).unwrap();
-    let mut pos: i32 = 50;
-    let part2 = input
+    let part2 = fs::read_to_string("input2.txt")
+        .unwrap()
         .trim()
         .split("\n")
         .flat_map(|x| {
@@ -31,12 +23,10 @@ fn main() {
             let off = x[1..].parse::<usize>().unwrap();
             std::iter::repeat_n(d, off)
         })
-        .map(|off| {
-            pos += off;
-            pos = pos.rem_euclid(100);
-            pos
+        .fold((50i32, 0), |(pos, count), off| {
+            let pos = (pos + off).rem_euclid(100);
+            (pos, count + if pos == 0 { 1 } else { 0 })
         })
-        .filter(|&x| x == 0)
-        .count();
+        .1;
     println!("{:?}", part2);
 }
