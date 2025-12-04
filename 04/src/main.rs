2dd5af62a111a3e1d0fc7c6b7d48f7b66cd8045a
cynic <daniel@szymanski.cc>
2025-12-04 08:35:44 +0100

    Tidy up day 04 a bit

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -1,33 +1,6 @@
 use std::{collections::HashSet, fs};
 
 fn main() {
-    let rolls = fs::read_to_string("input2.txt")
-        .unwrap()
-        .lines()
-        .enumerate()
-        .fold(HashSet::new(), |mut acc, (y, l)| {
-            l.trim().as_bytes().iter().enumerate().for_each(|(x, &b)| {
-                if b == b'@' {
-                    acc.insert((x as i64, y as i64));
-                }
-            });
-            acc
-        });
-    let part1 = rolls
-        .iter()
-        .filter(|(x, y)| {
-            let adj = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
-                .iter()
-                .filter(|dir| {
-                    let p = (x + dir.0, y + dir.1);
-                    rolls.contains(&p)
-                })
-                .count();
-            adj < 4
-        })
-        .count();
-    println!("part1 {:?}", part1);
-
     let mut rolls = fs::read_to_string("input2.txt")
         .unwrap()
         .lines()
@@ -41,29 +14,29 @@ fn main() {
             acc
         });
 
+    let part1 = adj(&rolls).count();
+    println!("part1 {:?}", part1);
+
     let mut count = 0;
-    let mut last = 0;
     loop {
-        let to_rm = rolls
-            .iter()
-            .filter(|(x, y)| {
-                let adj = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
-                    .iter()
-                    .filter(|dir| {
-                        let p = (x + dir.0, y + dir.1);
-                        rolls.contains(&p)
-                    })
-                    .count();
-                adj < 4
-            })
-            .cloned()
-            .collect::<HashSet<(i64, i64)>>();
-        count += to_rm.len();
-        if count == last {
+        let to_rm = adj(&rolls).cloned().collect::<HashSet<(i64, i64)>>();
+        if to_rm.is_empty() {
             break;
         }
-        last = count;
-        rolls = rolls.difference(&to_rm).cloned().collect();
+        count += to_rm.len();
+        to_rm.iter().for_each(|p| {
+            rolls.remove(p);
+        });
     }
     println!("part2 {:?}", count);
+}
+
+fn adj(rolls: &HashSet<(i64, i64)>) -> impl Iterator<Item = &(i64, i64)> {
+    rolls.iter().filter(|p| {
+        let adj = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
+            .iter()
+            .filter(|dir| rolls.contains(&(p.0 + dir.0, p.1 + dir.1)))
+            .count();
+        adj < 4
+    })
 }
